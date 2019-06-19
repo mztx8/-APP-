44ad6cd889c6eb564e96f06559f4f6209d718dc5
Mateo <39872643+mztx8@users.noreply.github.com>
2019-06-19 08:55:25 +0800

    Revert "影视源码双端后台"
    This reverts commit 84694be8eb89c4c00e5bc277d3c9eab664487fb0.

--- back-end/qq/zb_users/plugin/KODExplorer/KODExplorer/template/setting/slider/help.php
+++
@@ -1,6 +0,0 @@
-<div class='h1'><i class="font-icon icon-question"></i><?php echo $L['setting_help'];?>
-<div class="section">
-	<div class="content">
-		<?php include(LANGUAGE_PATH.LANGUAGE_TYPE.'/help.html');?>
-	</div>
-</div>
